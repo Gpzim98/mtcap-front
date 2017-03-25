42c8207927400495c70c80b1bac4e6ec7d4ff42d
gpzim98 <gpzim98@gmail.com>
2017-03-25 15:57:02 +0000

    Preparing the frontend app

---
+++ src/dev/banners/banners.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'banners',
+  templateUrl: 'banners.html',
+  styleUrls: ['style.css']
+})
+export class BannersComponent {
+  @Input() banners = [
+    {'url': 'link1', 'alt': 'my first slide'},
+    {'url': 'link2', 'alt': 'my second slide'},
+    {'url': 'link3', 'alt': 'my third slide'},
+  ];
+
+  constructor() {
+
+  }
+
+}
