bf4cf3340b9e837298df5b3f717c92cdcb7f1e4c
gpzim98 <gpzim98@gmail.com>
2017-04-11 00:40:55 +0100

    Finishing the page next-sortition

--- src/app/next-sortition/next-sortition.component.ts
+++ src/app/next-sortition/next-sortition.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeService } from './../home/home.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-next-sortition',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./next-sortition.component.css']
 })
 export class NextSortitionComponent implements OnInit {
+  nextSortition: any;
 
-  constructor() { }
+  constructor(public homeService: HomeService, private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
+      this.loadNextSortition();
   }
 
+  loadNextSortition() {
+      this.homeService.getNextSortition().subscribe(
+          data => {
+              this.nextSortition = data.results[0];
+      },
+      error => console.log('Erro getNextSortition ' + error),
+    );
+  }
+
+  transform(url) {
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
 }
