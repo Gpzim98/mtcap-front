08aca7746c2439cae5eb0caa796e8cbd1f8ace11
GP <gregorypacheco@Gregorys-MacBook-Air.local>
2017-07-30 17:10:10 +0100

    Set up project on mac

--- src/app/sortition/sortition.service.ts
+++ src/app/sortition/sortition.service.ts
@@ -8,15 +8,15 @@ export class SortitionService {
 
 
   getSortitionList() {
-      let url = 'https://mtcap.herokuapp.com/api/sortitions/?limit=500';
-      // let url = 'http://localhost:8000/api/sortitions/?limit=500';
+    //   let url = 'https://mtcap.herokuapp.com/api/sortitions/?limit=500';
+      let url = 'http://localhost:8000/api/sortitions/?limit=500';
       return this.http.get(url)
           .map(res => res.json());
   }
 
     getSortitions(id) {
-        let url = 'https://mtcap.herokuapp.com/api/sortitions/' + id;
-        // let url = 'http://localhost:8000/api/sortitions/' + id;
+        // let url = 'https://mtcap.herokuapp.com/api/sortitions/' + id;
+        let url = 'http://localhost:8000/api/sortitions/' + id;
         return this.http.get(url)
             .map(res => res.json());
     }
